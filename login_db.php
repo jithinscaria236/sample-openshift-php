76d0ac88b0755ee51d2adfd9c9b1b5b253c56541
jithinscaria236 <jithinscaria236@gmail.com>
2022-03-09 09:24:55 +0530

    Update login_db.php

--- login_db.php
+++ login_db.php
@@ -5,11 +5,4 @@ $dbpwd = getenv("MYSQL_PASSWORD");
 $dbname = getenv("MYSQL_DATABASE");
 $auidence = getenv("HELLO_AUDIENCE");
 $message= getenv("HELLO_MESSAGE");
-
-// Create connection
-$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed. Please make sure you have the MYSQL_SERVICE_HOST, MYSQL_USER, MYSQL_PASSWORD, and MYSQL_DATABASE environment variables : " . $conn->connect_error);
-}
 ?>
